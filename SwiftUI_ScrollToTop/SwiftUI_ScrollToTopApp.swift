6d223fe8b9473f07be881b9ce91a9be02d897ddc
SeonJungKim <ksj083895@gmail.com>
2021-04-26 22:17:06 +0500

    Initial Commit

---
+++ SwiftUI_ScrollToTop/SwiftUI_ScrollToTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ScrollToTopApp.swift
+//  SwiftUI_ScrollToTop
+//
+//  Created by 김선중 on 2021/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ScrollToTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
